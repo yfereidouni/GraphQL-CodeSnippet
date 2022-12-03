3950a1b189853351b00ad45fc6662a15efaa98ed
Yasser Fereidouni <yasser.fereidouni@gmail.com>
2022-12-03 16:36:30 +0100

    AppDbContext in Service container was registered.

--- CommanderGQL/Program.cs
+++ CommanderGQL/Program.cs
@@ -1,4 +1,13 @@
+using CommanderGQL.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("CommanderConStr"));
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
